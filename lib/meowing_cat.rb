0a95a368beceaca4ad6fd79a833b8d0ae688822d
Aashiyana Patel <aashiyanapatel@rocketmail.com>
2021-02-17 20:55:32 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,24 +1,2 @@
 class Cat
-  def name=(name)
-    @name = name
-  end
-  
-  def name
-    @name
-  end
-  
-  maru = Cat.new
-  maru.name = "Maru"
-  
-  def meow=(meow)
-    @meow = meow
-  end
-  
-  def meow
-    @meow
-  end
-  
-  maru = Cat.new
-  maru.meow = "meow!"
-end
 
